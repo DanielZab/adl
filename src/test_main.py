125a6f49a7a1f761557e768083f0d78e85095be3
Daniel Zabielski <e12119018@student.tuwien.ac.at>
2024-12-17 17:44:44 +0100

    Updated requirements and added test

--- src/test_main.py
+++ src/test_main.py
@@ -3,7 +3,7 @@ import logging, os, glob
 import datetime
 
 from dataset.containers import DataSet, DataPoint
-from util import extract_tickers, extract_from_pickle
+from util import extract_tickers, extract_from_pickle, get_datasets, get_train_validate_test_datasets
 
 
 DATA_PATH = os.path.join(".", "data")
@@ -25,3 +25,15 @@ def test_pickles_integrity():
         data = extract_from_pickle(f)
         assert isinstance(data, DataSet)
         assert all(isinstance(e, DataPoint) for e in data.data_points)
+
+def test_train_validation_test_split():
+    timestamp1 = datetime.datetime(2024, 2, 15)
+    timestamp2 = datetime.datetime(2024, 5, 1)
+    train_set, val_set, test_set = get_train_validate_test_datasets(get_datasets())
+    for e in train_set:
+        assert e <= timestamp1
+    for e in val_set:
+        assert e <= timestamp2 and e > timestamp1
+    for e in test_set:
+        assert e > timestamp2
+    
